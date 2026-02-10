aebdf5fcf9ebc199471a5e46c7803c0c96da8c6c
AlexEP11 <aleeps2911@gmail.com>
2026-02-10 12:29:11 -0600

    Adds new config to the player (ffmpeg)

--- src/configs/bot.config.ts
+++ src/configs/bot.config.ts
@@ -1,9 +1,26 @@
 import { Client, Collection, GatewayIntentBits } from "discord.js";
 import { deployGlobalCommands, deployGuildCommands } from "utils/deploy.util";
-import { loadCommands, loadEvents } from "utils/load.util";
+import {
+    loadCommands,
+    loadEvents,
+    loadPlayerExtractors,
+} from "utils/load.util";
 import { env } from "./env.config";
+import { Player } from "discord-player";
 
-export const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+export const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+export const player = new Player(client, {
+    skipFFmpeg: false,
+});
+
 client.commands = new Collection();
 
 export async function start() {
@@ -11,6 +28,7 @@ export async function start() {
     await loadEvents();
     await deployGuildCommands(commands);
     //*: Descomentar para produccion
-    // await deployGlobalCommands(commands);
+    await deployGlobalCommands(commands);
+    await loadPlayerExtractors();
     await client.login(env.TOKEN);
 }
