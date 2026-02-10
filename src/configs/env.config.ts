bec9783d7404ae2ce49727b54e245ca1bcac2bc8
AlexEP11 <aleeps2911@gmail.com>
2026-02-10 12:29:29 -0600

    Adds a new env var

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -5,4 +5,5 @@ export const env = {
     TOKEN: get("TOKEN").required().asString(),
     CLIENT_ID: get("CLIENT_ID").required().asString(),
     GUILD_ID: get("GUILD_ID").required().asString(),
+    CK: get("CK").required().asString(),
 };
