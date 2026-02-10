2a415f0c213c49b4abb3b011f41fd0fa5eafcd35
AlexEP11 <aleeps2911@gmail.com>
2026-02-10 12:26:01 -0600

    Update the loggers in deploy functions

--- src/utils/deploy.util.ts
+++ src/utils/deploy.util.ts
@@ -8,32 +8,36 @@ import {
 } from "discord.js";
 
 export async function deployGuildCommands(
-    commands: RESTPostAPIApplicationCommandsJSONBody[]
+    commands: RESTPostAPIApplicationCommandsJSONBody[],
 ) {
     try {
-        Logger.info(`Refreshing ${commands.length} guild commands...`);
+        Logger.info(`⚙️  Refreshing ${commands.length} guild commands...`);
         const data = (await rest.put(
             Routes.applicationGuildCommands(env.CLIENT_ID, env.GUILD_ID),
-            { body: commands }
+            { body: commands },
         )) as RESTPostAPIApplicationCommandsJSONBody[];
 
-        Logger.success(`Successfully reloaded ${data.length} guild commands.`);
+        Logger.success(
+            `♻️  Successfully reloaded ${data.length} guild commands.`,
+        );
     } catch (error) {
         Logger.error(error);
     }
 }
 
 export async function deployGlobalCommands(
-    commands: RESTPostAPIChatInputApplicationCommandsJSONBody[]
+    commands: RESTPostAPIChatInputApplicationCommandsJSONBody[],
 ) {
     try {
-        Logger.info(`Refreshing ${commands.length} global commands...`);
+        Logger.info(`⚙️  Refreshing ${commands.length} global commands...`);
         const data = (await rest.put(
             Routes.applicationCommands(env.CLIENT_ID),
-            { body: commands }
+            { body: commands },
         )) as RESTPostAPIApplicationCommandsJSONBody[];
 
-        Logger.success(`Successfully reloaded ${data.length} global commands.`);
+        Logger.success(
+            `♻️  Successfully reloaded ${data.length} global commands.`,
+        );
     } catch (error) {
         Logger.error(error);
     }
